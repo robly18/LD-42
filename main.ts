a8484af610476c4909ff1461741bfd46d8c1d873
robly18 <duarteluis.nascimento@hotmail.com>
2018-08-19 19:02:04 +0100

    fixed

--- main.ts
+++ main.ts
@@ -1,9 +1,5 @@
 window.onload = function() {
-  //let game : Game = new Game(document.getElementById('canvas') as HTMLCanvasElement);
-  //game.start();
+  let game : Game = new Game(document.getElementById('canvas') as HTMLCanvasElement);
+  game.start();
 
-  let map : Map = new Map(100,100);
-  for(let i = 0; i < 100; i++)
-    for(let j = 0; j < 100; j++)
-      console.log(map.ground[i][j]);
 }
