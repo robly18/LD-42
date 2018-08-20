1815d2aa56f7cc0f7e23a87f49ad51eae4765803
robly18 <duarteluis.nascimento@hotmail.com>
2018-08-20 15:42:13 +0100

    made player be in foreground

--- src/asteroid.ts
+++ src/asteroid.ts
@@ -9,21 +9,21 @@ class Asteroid {
     this.player = new Entity(new Point(100,100), false);
     this.player.movement = new PlayerMovementComponent();
     this.player.graphics = new CreatureGraphicsComponent("assets/player.png");
-    this.entities.push(this.player);
   }
 
   public tick(data : GameData) {
     for (let e of this.entities) {
       e.tick(data, this);
     }
+    player.tick(data, this);
   }
 
   public render(data : GameData, cam : Point) {
-    let ctx = data.ctx;
-    this.map.render(data, cam);
-
+    this.map.render_background(data, cam);
     for (let e of this.entities) {
       e.render(data, cam);
     }
+    player.render(data,cam);
+    this.map.render_foreground(data, cam);
   }
 }
