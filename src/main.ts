5afbf5706a530dd04de5dff077cb65ef4323f0ce
João Tavares <jcostavares@gmail.com>
2018-08-23 01:33:10 +0100

    balance

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ const BUILDING_RANGE = 100;
 const TICKS_PER_MINE = 60;
 const GROUND_MAX_VALUE = 32;
 
-const CONSTRUCTION_PARTS_RECIPE : [number, number, number] = [5, 0, 1/16];
+const CONSTRUCTION_PARTS_RECIPE : [number, number, number] = [3, 0, 1/16];
 const CONSTRUCTION_PARTS_TIME = 100;
-const FUEL_RECIPE : [number, number, number] = [0,1,1/256];
+const FUEL_RECIPE : [number, number, number] = [0,2,1/256];
 const FUEL_TIME = 420;
 
 const ASTEROID_VELOCITY = 400/1000;
